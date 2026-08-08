38d40091ecfb829bc971e489df44290b8ac413b3
Jack Wallner <jackwallner@gmail.com>
2026-08-08 16:18:37 -0700

    fix: use the football App Store review URL

--- StatScout/Utilities/AppStoreReviewLinks.swift
+++ StatScout/Utilities/AppStoreReviewLinks.swift
@@ -3,7 +3,7 @@ import StoreKit
 
 /// App Store review deep links for StatScout.
 enum AppStoreReviewLinks {
-    static let appStoreID = "6743780853"
+    static let appStoreID = "6792930447"
 
     /// Opens the App Store write-review page in the user's storefront.
     static var writeReviewURL: URL {
